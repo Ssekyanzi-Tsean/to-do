2796227146efcc2a467a7d9acfe040acb9097863
Shogun <msean481@gmail.com>
2021-02-15 10:48:27 -0500

    a little Changes taking a break

--- to-do/src/components/Form.js
+++ to-do/src/components/Form.js
@@ -4,7 +4,7 @@
 import React from "react";
 
 
-const Form = ({ setInputText,todos,setTodos, inputText}) => {
+const Form = ({ setInputText,todos,setTodos, inputText, setStatus}) => {
 
   const inputTextHandler = (e) => {
     console.log(e.target.value);
@@ -18,7 +18,7 @@ const Form = ({ setInputText,todos,setTodos, inputText}) => {
     setInputText("")
   };
   const statusHandler = (e) => {
-    console.log(e);
+    setStatus(e.target.value);
   }
     return(
         <form>
@@ -27,7 +27,7 @@ const Form = ({ setInputText,todos,setTodos, inputText}) => {
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
